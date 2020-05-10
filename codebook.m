80e6bad6895475b3d103ed75e455b1df2c7afe66
jagrevel <65041302+jagrevel@users.noreply.github.com>
2020-05-10 02:52:30 -0400

    Create codebook.m

---
+++ codebook.m
@@ -0,0 +1,29 @@
+Variable description
+
+
+__Datasets summary:__
+
+Name:				Type:		Obs.		Variables 		Purpose
+activiyCodes 		tibble 		6 			2				Activity lookup table
+averages			tibble 		10,299		68				Data set satisfying condition 4 of assignment
+combined			tibble 		10,299		560				Data set satisfying condition 1- of assignment
+groupedSummary		tibble 		180			68				Data set satisfying condition 5 of assignmnet
+testers 			tibble 		2,947		1				Imported test subject list
+testX 	 			tibble 		2,947		560				Imported test dataset
+testY 	 			tibble 		2,947		1				Imported test activity list
+trainers 	 		tibble 		7,352		1				Imported train subject list
+trainX 	 			tibble 		7,352		560				Imported train dataset
+trainY 	 			tibble 		7,352		1				Imported train activity list
+features			data.frame	560			1				Imported variable names
+
+
+
+
+
+x :  int 3
+folder :  chr "./UCI HAR Dataset"
+libList :  chr [1:3] "dplyr" "readr" "stringr"
+packs : 'data.frame':	3 obs. of  2 variables:
+ $ packageName: Factor w/ 3 levels "dplyr","readr",..: 1 2 3
+ $ installed  : logi  TRUE TRUE TRUE
+ target :  chr
